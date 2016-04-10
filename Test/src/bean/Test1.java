3fdf15c3765d17cd5cf69b292bee4eec8cf7992b
yangwei <513215275@qq.com>
2016-04-10 15:43:28 +0800

    james add code

--- Test/src/bean/Test1.java
+++ Test/src/bean/Test1.java
@@ -2,6 +2,6 @@ package bean;
 
 public class Test1 {
 	public void caocaocao(){
-		System.out.println("this is master code,and tom coding");
+		System.out.println("this is master code,and james coding");
 	}
 }
